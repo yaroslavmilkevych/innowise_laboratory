4a1406434d610c5d648da79f5557aba0496bdc8e
my_apple <dell@MacBook-Pro.local>
2025-11-12 19:53:00 +0100

    Fix typo in Hello World message

--- lecture_1/main.py
+++ lecture_1/main.py
@@ -4,7 +4,7 @@ from colorama import init, Fore, Back, Style
 init()
 
 #Print colored Hello World
-print(f"{Fore.RED}{Back.YELLOW}Hellow World!{Style.RESET_ALL}")
+print(f"{Fore.RED}{Back.YELLOW}Hello World!{Style.RESET_ALL}")
 print(f"{Fore.GREEN}Hello World in Green!{Style.RESET_ALL}")
 print(f"{Fore.BLUE}{Style.BRIGHT}Hello World in Bright Blue!{Style.RESET_ALL}")
 print(f"{Fore.MAGENTA}{Back.CYAN}Hello World with Magenta text and Cyan background!{Style.RESET_ALL}")
